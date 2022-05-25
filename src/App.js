23a99768272a65f18db80846a2bd7f878499fc0d
Jorge Andrés Choque <jorge.ch.999santos@gmail.com>
2022-05-25 04:04:01 -0300

    example.env agregado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  // state de la app
+  const [ busqueda, guardarBusqueda ] = useState('');
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (busqueda === '') return;
+  
+      const imagenesPorPagina = 10;
+      const key = process.env.REACT_APP_APIKEY;
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarBusqueda(resultado.hits);
+      console.log(resultado.hits);
+    }
+    consultarAPI();
+  }, [busqueda]);
+
   return (
     <div className='container'>
       <div className='jumbotron'>
         <p className='lead text-center'>Buscador de Imagenes</p>
-        <Formulario />
+        <Formulario 
+          guardarBusqueda={guardarBusqueda}
+        />
       </div>
     </div>
   );
