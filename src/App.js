c240da7213b8771b50afaa13b8445e9fd0e9e050
Jorge Andrés Choque <jorge.ch.999santos@gmail.com>
2022-05-25 07:29:31 -0300

    Componentes de Listado Agregados

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import ListadoImagenes from './components/ListadoImagenes';
 
 function App() {
 
   // state de la app
   const [ busqueda, guardarBusqueda ] = useState('');
+  const [ imagenes, guardarImagenes ] = useState([]);
 
   useEffect(() => {
     const consultarAPI = async () => {
       if (busqueda === '') return;
   
-      const imagenesPorPagina = 10;
+      const imagenesPorPagina = 30;
       const key = process.env.REACT_APP_APIKEY;
       const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
 
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
 
-      guardarBusqueda(resultado.hits);
-      console.log(resultado.hits);
+      guardarImagenes(resultado.hits);
     }
     consultarAPI();
   }, [busqueda]);
@@ -29,6 +30,12 @@ function App() {
         <p className='lead text-center'>Buscador de Imagenes</p>
         <Formulario 
           guardarBusqueda={guardarBusqueda}
+        />
+      </div>
+
+      <div className='row justify-content-center'>
+        <ListadoImagenes
+          imagenes={imagenes}
         />
       </div>
     </div>
