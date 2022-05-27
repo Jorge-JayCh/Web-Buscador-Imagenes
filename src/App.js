72277ef5a12e2e240b4b569138aaaaea5a54a692
Jorge Andrés Choque <jorge.ch.999santos@gmail.com>
2022-05-27 07:07:39 -0300

    Paginación Agregada

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   // state de la app
   const [ busqueda, guardarBusqueda ] = useState('');
   const [ imagenes, guardarImagenes ] = useState([]);
+  const [ paginaactual, guardarPaginaActual ] = useState(1);
+  const [ totalpaginas, guardarTotalPaginas ] = useState(1);
 
   useEffect(() => {
     const consultarAPI = async () => {
@@ -14,15 +16,39 @@ function App() {
   
       const imagenesPorPagina = 30;
       const key = process.env.REACT_APP_APIKEY;
-      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaactual}`;
 
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
 
       guardarImagenes(resultado.hits);
+      // console.log(resultado);
+
+      // calcular el total de paginas
+      const calcularTotalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina);
+      guardarTotalPaginas(calcularTotalPaginas);
+
+      // Desplazar la pantalla hacia arriba
+      const $jumbotron = document.querySelector('.jumbotron');
+      $jumbotron.scrollIntoView({ behavior: 'smooth'})
+
     }
     consultarAPI();
-  }, [busqueda]);
+  }, [busqueda, paginaactual]);
+
+  // definir la página anterior
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaactual - 1;
+    if ( nuevaPaginaActual === 0 ) return;
+    guardarPaginaActual(nuevaPaginaActual);
+  }
+
+    // definir la página Siguiente
+    const paginaSiguiente = () => {
+      const nuevaPaginaActual = paginaactual + 1;
+      if ( nuevaPaginaActual > totalpaginas ) return;
+      guardarPaginaActual(nuevaPaginaActual);
+    }
 
   return (
     <div className='container'>
@@ -37,6 +63,24 @@ function App() {
         <ListadoImagenes
           imagenes={imagenes}
         />
+        { (paginaactual === 1) 
+            ? null 
+            : <button
+                type='button'
+                className='bbtn btn-info mr-1'
+                onClick={paginaAnterior}
+              >&laquo; Anterior </button>
+        }
+
+        { (paginaactual === totalpaginas) 
+            ? null 
+            : <button
+                type='button'
+                className='bbtn btn-info'
+                onClick={paginaSiguiente}
+              >Siguiente &raquo;</button>
+        }
+
       </div>
     </div>
   );
